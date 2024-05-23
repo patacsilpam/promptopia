1f8c04139976d0929b2b6ae7f9ca0a0f00e59577
patacsilpam <pamelampatacsil@gmail.com>
2024-05-23 21:51:46 +0800

    main page or home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Discover & Share
+        <br/>
+        <span className="orange_gradient text-center">
+          AI Powered Prompts
+        </span>
+      </h1>
+    </section>
   )
 }
 
