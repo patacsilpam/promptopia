324d15932c1e32ea1d144ecf0244a6cacf631e24
patacsilpam <pamelampatacsil@gmail.com>
2024-05-25 21:10:07 +0800

    feat: home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from '@components/Feed';
+
 const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
@@ -8,6 +10,9 @@ const Home = () => {
           AI Powered Prompts
         </span>
       </h1>
+      <p className="desc text-center">Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts.</p>
+    {/**FEED COMPONENT */}
+    <Feed/>
     </section>
   )
 }
